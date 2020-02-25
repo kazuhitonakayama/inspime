4c89379a6ed6d986dc238db2daca5e9d631afe00
kazuhitonakayama <firstjin19990101@gmail.com>
2020-02-25 18:44:23 +0900

    TOPのルーティング記載

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  get '/' => 'home#top' # Rootディレクトリ
+  get '/' => 'photos#index' # Rootディレクトリ
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   post 'photos' => 'photos#create'
   get 'photos' => 'photos#index'
   get 'photos/new' => 'photos#new'
-  
+
 end
